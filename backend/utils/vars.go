b82591f7bd142e57185911ca55a7518fd0974916
Nicolas Beauvais <nicolas@bvs.email>
2025-08-04 19:47:43 +0200

    Increase default log retention to 30 days

--- backend/utils/vars.go
+++ backend/utils/vars.go
@@ -26,7 +26,7 @@ func init() {
 	TcpPort = GetSanitizedEnvString("SLOGGO_TCP_PORT", "6514")
 	ApiPort = GetSanitizedEnvString("SLOGGO_API_PORT", "8080")
 
-	LogRetentionMinutes = GetSanitizedEnvInt64("SLOGGO_LOG_RETENTION_MINUTES", 3*24*60) // Default to 3 days
+	LogRetentionMinutes = GetSanitizedEnvInt64("SLOGGO_LOG_RETENTION_MINUTES", 30*24*60) // Default to 30 days
 }
 
 func GetSanitizedEnvString(key string, defaultValue string) string {
